137439fd63e9a7d62807693926119842f2c14b1e
Guilherme Vicentin <gvicentin@ciandt.com>
2022-02-04 18:45:55 -0300

    Project setup

---
+++ desktop/src/com/vgr/pa/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.vgr.pa.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.vgr.pa.PrisonApocalypse;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Prison Apocalypse";
+		config.width = 1024;
+		config.height = 720;
+		new LwjglApplication(new PrisonApocalypse(), config);
+	}
+}
